16d0358d52abdc525f5d188ec7f40636fbe14521
Erik Schultink <erik@worklytics.co>
2023-01-24 16:05:20 -0800

    basic example of using module

---
+++ examples/basic/main.tf
@@ -0,0 +1,9 @@
+
+# basic example of using this module; really as much for dev/testing as a real example of practical
+# usage
+
+module "worklytics_export" {
+  source = "../../"
+
+  worklytics_tenant_id = "123456712345671234567"
+}
