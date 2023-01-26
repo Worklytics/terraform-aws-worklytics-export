c3bc94ffd0e46ee228c0c1ac9d70e6c1b8bb6d3a
Erik Schultink <erik@worklytics.co>
2023-01-25 20:04:25 -0800

    export cli creds

--- test/rsync.sh
+++ test/rsync.sh
@@ -14,7 +14,19 @@ CI_RUN=`date +%Y%m%d'T'%H%M%S`
 # that can impersonate the target service account
 GCP_TOKEN=`gcloud auth print-identity-token --impersonate-service-account=${EXAMPLE_TENANT_SA_EMAIL}`
 
-aws sts assume-role-with-web-identity --role-arn IAM_ROLE_ARN --role-session-name "ci-run-${CI_RUN}" --web-identity-token $GCP_TOKEN --provider-id "accounts.google.com" > /tmp/assume-role.json
+echo "BUCKET_NAME: ${IAM_ROLE_ARN}\n"
+echo "IAM_ROLE_ARN: ${IAM_ROLE_ARN}\n"
+
+export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
+$(aws sts assume-role-with-web-identity \
+--role-arn $IAM_ROLE_ARN \
+--role-session-name "ci-run-${CI_RUN}" \
+--web-identity-token $GCP_TOKEN \
+--provider-id "accounts.google.com" \
+--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
+--output text))
+
+# NOTE : presuming that it's OK we overwrote prior AWS env vars, bc next gh action run will reset
 
 # if bucket exists + perms OK, this should work
 aws s3 sync . s3://$BUCKET_NAME/${CI_RUN}
