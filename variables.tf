f7c53f1ee4009357d7d3e10bd251e62bb3fcf346
Erik Schultink <erik@worklytics.co>
2023-03-24 07:11:53 -0700

    avoid changing module variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-variable "aws_account_id" {
-  type        = string
-  description = "AWS account in which to provision. Required to be explicitly specified, to reduce chance of inadvertently provisioning in the wrong account."
-}
-
-variable "aws_role_name" {
-  type        = string
-  description = "The name of the role to assume within AWS account. `null` if already auth'd as desired role/user."
-  default     = null
-}
-
 variable "resource_name_prefix" {
   type        = string
   description = "Prefix to give to names of infra created by this module, where applicable."
