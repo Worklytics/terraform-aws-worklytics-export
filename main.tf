3affe54626f37873892999f92117bb750d672c2b
davidfq <davidfq@gmail.com>
2023-05-18 18:20:43 +0200

    Update TODO file contents

--- main.tf
+++ main.tf
@@ -110,12 +110,28 @@ resource "aws_iam_policy_attachment" "allow_worklytics_tenant_bucket_access" {
 
 locals {
   todo_content = <<EOT
-# Configure Export in Worklytics
+# Configure Data Export in Worklytics
 
-  1. Go to [https://app.worklytics.co/](https://app.worklytics.co/), navigate to 'Export Data' and
-     'Create new connection'.
-  2. Set `Location` to `s3://${aws_s3_bucket.worklytics_export.bucket}`
-  3. Set `Role` to `${aws_iam_role.for_worklytics_tenant.arn}`
+1. Ensure you're authenticated with Worklytics. Either sign-in at [https://app.worklytics.co](https://app.worklytics.co)
+  with your organization's SSO provider *or* request OTP link from your Worklytics support team.
+2. Visit `https://app.worklytics.co/analytics/data-export/connect?bucket=${aws_s3_bucket
+.worklytics_export.bucket}&roleArn=${aws_iam_role.for_worklytics_tenant.arn}`
+3. Review any additional settings (such as the Dataset type you'd like to export) and adjust
+  values as you see fit, then click "Create Data Export".
+
+Alternatively, you may follow the manual instructions below:
+
+1. Visit [https://app.worklytics.co/analytics/data-export](https://app.worklytics.co/analytics/data-export)
+  (or login into Worklytics, and navigate to Manage --> Export Data).
+2. Click on the 'Create New Data Export' button in the upper right.
+3. Fill in the form with the following values:
+  - **Data Export Name** - choose a name that will help you identify this export in the future.
+  - **Data Export Type** - choose the type of data you'd like to export. Check our
+    [Data Export Documentation](https://app.worklytics.co/docs/data-export) for a complete
+    description of all the available datasets.
+  - **Data Destination** - choose 'Amazon S3', use `${aws_s3_bucket.worklytics_export.bucket}`
+    for the **Bucket** field, and `${aws_iam_role.for_worklytics_tenant.arn}` for the **Role ARN**
+    field.
 
 EOT
 }
