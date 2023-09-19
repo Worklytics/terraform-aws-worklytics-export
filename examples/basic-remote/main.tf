1381b5465cfa303eeddaf097de5915c318ebf242
Erik Schultink <erik@worklytics.co>
2023-09-19 14:44:05 -0700

    update remote example

--- examples/basic-remote/main.tf
+++ examples/basic-remote/main.tf
@@ -8,10 +8,13 @@ terraform {
 
 module "worklytics_export" {
   source  = "terraform-aws-worklytics-export"
-  version = "~> 0.3.0"
+  version = "~> 0.4.0"
 
   resource_name_prefix = var.resource_name_prefix
   worklytics_tenant_id = var.worklytics_tenant_id
+  worklytics_host      = var.worklytics_host
+  todos_as_outputs     = var.todos_as_outputs
+  todos_as_local_files = var.todos_as_local_files
 }
 
 output "worklytics_export_bucket_id" {
